5819a1dea43be30c971c31973cac1b9bf9cb12cc
Justin Smith <Justin-Smith@users.noreply.github.com>
2021-11-28 21:06:26 -0500

    Create ItemController.cs

---
+++ WishList/Controllers/ItemController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+using WishList.Data;
+
+namespace WishList.Controllers
+{
+    public class ItemController
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ItemController(ApplicationDbContext context){
+            _context = context;
+        }
+    }
+}
